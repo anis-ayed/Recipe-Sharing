2b0a6f42046a892b8291a20d07738f84f2a7e7b6
anis ayed <ayed.aniss@hotmail.com>
2024-03-16 12:49:45 +0100

    First commit

---
+++ src/main/java/com/ayed/recipe_sharing/RecipeSharingApplication.java
@@ -0,0 +1,13 @@
+package com.ayed.recipe_sharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeSharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeSharingApplication.class, args);
+    }
+
+}
